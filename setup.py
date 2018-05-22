01f0df96cbf820f4e5cefe50af189209c14e340d
Sami Moustachir <moustachir.sami@gmail.com>
2018-05-22 16:49:43 +0200

    version pypi

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def setup_package():
         author='sami moustachir',
         author_email='moustachir.sami@gmail.com',
         url='https://github.com/sammous/spacy-lefff',
-        version='0.1.3',
+        version='0.2.0',
         license='MIT',
         packages=find_packages(exclude=['tests']),
         package_data={'spacy_lefff': ['data/*']},
