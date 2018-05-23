0db3e52a8a1e2bf54e66d6a9d43bea9553851f5a
Sami Moustachir <moustachir.sami@gmail.com>
2018-05-23 14:37:02 +0200

    improving code cov

--- tests/test_pos_tagger.py
+++ tests/test_pos_tagger.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 from __future__ import unicode_literals
 from spacy_lefff import POSTagger, LefffLemmatizer
+from spacy_lefff.melt_tagger import MODELS_DIR
 
 import pytest
 import spacy
+import os
 
 @pytest.fixture
 def nlp():
@@ -38,3 +40,17 @@ def test_lemmatizer_verb(add_lefff_lemma_nlp):
 def test_lemmatizer_noun(add_lefff_lemma_nlp):
     tokens = add_lefff_lemma_nlp(u"il y a des Françaises.")
     assert tokens[4]._.lefff_lemma == u"français"
+
+def test_load_lexicon():
+    french_pos_tagger = POSTagger()
+    lex_dict = french_pos_tagger.lex_dict
+    lexicon = os.path.join(MODELS_DIR, 'lexicon.json')
+    french_pos_tagger.load_lexicon(lexicon)
+    assert french_pos_tagger.lex_dict == lex_dict
+
+def test_load_tag():
+    french_pos_tagger = POSTagger()
+    tag_dict = french_pos_tagger.tag_dict
+    tag = os.path.join(MODELS_DIR, 'tag_dict.json')
+    french_pos_tagger.load_lexicon(tag)
+    assert french_pos_tagger.tag_dict == tag_dict
