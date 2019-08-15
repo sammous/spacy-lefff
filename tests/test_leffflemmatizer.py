ea30b95a9aa5152026f2d7458692f7ddf949dce0
Sami Moustachir <moustachir.sami@gmail.com>
2019-08-15 14:52:19 +0800

    tmp fix breakline

--- tests/test_leffflemmatizer.py
+++ tests/test_leffflemmatizer.py
@@ -1,6 +1,4 @@
 # coding: utf-8
-
-
 import pytest
 
 import spacy
@@ -17,27 +15,34 @@ def test_lemmatizer_verb(nlp):
     tokens = nlp(u"J'ai une maison à Paris.")
     assert tokens[1]._.lefff_lemma == u"avoir"
 
+
 def test_lemmatizer_noun_verb(nlp):
     tokens = nlp(u"Les abaissements de température sont gênants.")
     assert tokens[1]._.lefff_lemma == u"abaissement"
 
+
 def test_lemmatizer_noun(nlp):
     tokens = nlp(u"il y a des Françaises.")
     assert tokens[4]._.lefff_lemma == u"français"
+
 
 def test_lemmatizer_noun_2(nlp):
     tokens = nlp(u"Les abaissements de température sont gênants.")
     assert tokens[1]._.lefff_lemma == u"abaissement"
     assert tokens[3]._.lefff_lemma == u"température"
 
+
 def test_punctuations(nlp):
     tokens = nlp(u". ?")
     assert tokens[0]._.lefff_lemma == u"."
     assert tokens[1]._.lefff_lemma == u"?"
+
+
 @pytest.mark.exception
 def test_lemmatizer_exception():
     french_lemmatizer = LefffLemmatizer()
     assert french_lemmatizer.lemmatize(u"unknow34", u"unknown") is None
+
 
 def test_lemmatizer_default():
     french_lemmatizer = LefffLemmatizer(default=True)
